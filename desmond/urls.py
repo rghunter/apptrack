f78ae2eb8f69ae17ff0d7cc60d7e86ffb77e518c
Ryan G. Hunter <ryan@cens.io>
2017-10-22 21:02:35 -0400

    redirect root to apptrack

--- desmond/urls.py
+++ desmond/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('apptrack/')),
     url(r'^admin/', admin.site.urls),
     url(r'^apptrack/', include('apptrack.urls'))
 ]
